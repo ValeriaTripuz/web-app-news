63d028ee4669760a503fcec4a88559beaecbe3cb
Valeria <tripuzvaleria@gmail.com>
2023-09-04 17:20:30 +0300

    test request

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
 import React from "react";
 import "./App.css";
-// import Button from "@mui/material/Button";
 import Header from "./components/Header";
 import Main from "./components/Main";
 import Footer from "./components/Footer";
+import { useQuery } from "react-query";
+import axios from "axios";
+
+export async function fetchCoins<T>(): Promise<T> {
+  const { data } = await axios.get(
+    `http://api.mediastack.com/v1/news?access_key=8c824115399002747b3a044ccc2d086e`
+  );
+  return data;
+}
 
 function App() {
+  const data = useQuery("answer", fetchCoins);
+
+  console.log(data);
   return (
     <div className="App">
       <Header />
